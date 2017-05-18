9a13b18e53324cdb8d809dde76a85dbf8bbfd4f2
Kyle Bonar <kylewb2011@gmail.com>
2017-05-18 18:32:40 -0500

    Configured bare-bones router setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Nav from './Nav'
+import About from './About'
+import Home from './Home'
+import Projects from './Projects'
+import Resume from './Resume'
+
 
 class App extends React.Component {
 	render() {
 		return(
-			<div> Hi!! </div>
+			<BrowserRouter>
+				<div className="container">
+					<Nav />
+					<Route path="/about" component={About} />
+					<Route path="/home" component={Home} />
+					<Route path="/projects" component={Projects} />
+					<Route path="/resume" component={Resume} />
+				</div>
+			</BrowserRouter>
 		)
 	}
 }
