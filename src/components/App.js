f67556874381cab956963bff1785bab31fc1b3bb
Kyle Bonar <kylewb2011@gmail.com>
2017-05-21 10:12:20 -0500

    Removed route not being used.

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,6 @@ class App extends React.Component {
 					<Nav />
 					<Route exact path="/" component={About} />
 					<Route path="/about" component={About} />
-					<Route path="/home" component={Home} />
 					<Route path="/projects" component={Projects} />
 					<Route path="/resume" component={Resume} />
 				</div>
