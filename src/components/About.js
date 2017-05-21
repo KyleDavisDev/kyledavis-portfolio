b8e34a53f351bb89a61e1be56ff4466e7beac9f9
Kyle Bonar <kylewb2011@gmail.com>
2017-05-21 08:57:57 -0500

    Added content.

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,27 @@ import React from 'react';
 class About extends React.Component {
 	render() {
 		return(
-			<div> About</div>
+			<div className="about-container">
+				<p className="droid-reg">					
+					<span className="droid-bold">Hello world!</span> Welcome to my portfolio website.
+				</p>
+
+				<p className="droid-reg">
+					I am a <span className="droid-bold">Full Stack Dev</span> who is working with JavaScript as much as possible.
+				</p>
+
+				<p>
+					I primarily focus on the front-end, I like to make pixel-perfect, minimalistic designs, but I also enjoy getting dirty on the backend with databases and server logic. 
+				</p>
+
+				<p>
+					The majority of the updates to this site will be focused on the "Projects" section. Stay tuned!
+				</p>
+
+				<p>
+					Thanks, Kyle Bonar
+				</p>
+			</div>
 		)
 	}
 }
