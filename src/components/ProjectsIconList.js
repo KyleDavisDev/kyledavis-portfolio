f7a04a065f34d94b8356f41396678dcd2f668d4d
Kyle Bonar <kylewb2011@gmail.com>
2017-05-22 12:14:23 -0500

    Added key. Restructure of state. Added link to new component.

--- src/components/ProjectsIconList.js
+++ src/components/ProjectsIconList.js
@@ -7,15 +7,15 @@ class ProjectsIconList extends Component {
 
 		this.state = {
 			icons: [
-				{ name: "Example 1", imageURL: "/src/images/example.png", title:"Example Title"},
-				{ name: "Example 2", imageURL: "/src/images/example.png", title:"Example Title"},
-				{ name: "Example 3", imageURL: "/src/images/example.png", title:"Example Title"},
-				{ name: "Example 4", imageURL: "/src/images/example.png", title:"Example Title"},
-				{ name: "Example 5", imageURL: "/src/images/example.png", title:"Example Title"},
-				{ name: "Example 6", imageURL: "/src/images/example.png", title:"Example Title"},
-				{ name: "Example 7", imageURL: "/src/images/example.png", title:"Example Title"},
-				{ name: "Example 8", imageURL: "/src/images/example.png", title:"Example Title"},
-				{ name: "Example 9", imageURL: "/src/images/example.png", title:"Example Title"}
+				{ displayName: "Department Website", displayImageURL: "/src/images/example.png", title:"Example Title", linkURL: "tamu_physics"},
+				{ displayName: "Example 2", displayImageURL: "/src/images/example.png", title:"Example Title", linkURL: "/"},
+				{ displayName: "Example 3", displayImageURL: "/src/images/example.png", title:"Example Title", linkURL: "/"},
+				{ displayName: "Example 4", displayImageURL: "/src/images/example.png", title:"Example Title", linkURL: "/"},
+				{ displayName: "Example 5", displayImageURL: "/src/images/example.png", title:"Example Title", linkURL: "/"},
+				{ displayName: "Example 6", displayImageURL: "/src/images/example.png", title:"Example Title", linkURL: "/"},
+				{ displayName: "Example 7", displayImageURL: "/src/images/example.png", title:"Example Title", linkURL: "/"},
+				{ displayName: "Example 8", displayImageURL: "/src/images/example.png", title:"Example Title", linkURL: "/"},
+				{ displayName: "Example 9", displayImageURL: "/src/images/example.png", title:"Example Title", linkURL: "/"}
 			]
 		}
 	}
@@ -24,11 +24,18 @@ class ProjectsIconList extends Component {
 		return(
 			<div className="projects-icon-list-container">
 				{this.state.icons.map( (icon) =>	
-						<NavLink className="project-card" activeClassName="active" to="/">
-							<img src={icon.imageURL} style={{height:75, width: 75}} title={icon.title} />
-							{icon.name}
+						<NavLink
+							key={icon.displayName}
+							className="project-card"
+							to={{
+								pathname: this.props.match.url + '/details',
+								search: '?title=' + icon.linkURL 
+							}}>
+							<img src={icon.displayImageURL} style={{height:200, width: 200}} title={icon.title} />
+							{icon.displayName}
 						</NavLink>			
 				)}
+
 			</div>
 		)
 	}
