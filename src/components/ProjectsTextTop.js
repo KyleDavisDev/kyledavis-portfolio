03b2dcdad20bae8f9d3d176ca1f57a7bc27265c7
Kyle Bonar <kylewb2011@gmail.com>
2017-05-22 12:14:47 -0500

    Fixed link

--- src/components/ProjectsTextTop.js
+++ src/components/ProjectsTextTop.js
@@ -8,7 +8,7 @@ class ProjectsTextTop extends Component {
 				<h2 className="droid-bold text-center"> My Projects </h2>
 
 				<p className="droid-reg">
-					Check out some of the projects that I've worked on over the years. Clicking any of the images will take you to a page with more information on the project. You can also visit my <Link to="https://github.com/KyleBonar">GitHub</Link> to checkout the source code for some of these.
+					Check out some of the things that I've created or have been a part of over the years. Clicking any of the images will take you to a page with more information on the project. You can also visit my <a className="inline-link" href="https://github.com/KyleBonar">GitHub</a> to checkout the source code for some of these.
 				</p>
 			</div>
 		)
