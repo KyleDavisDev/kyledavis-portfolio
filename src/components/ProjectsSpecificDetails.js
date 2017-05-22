eb0656e5353aaf063e1ca6a48bd1c52ef6146413
Kyle Bonar <kylewb2011@gmail.com>
2017-05-22 12:17:15 -0500

    Initial commit

---
+++ src/components/ProjectsSpecificDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ProjectsSpecificDetails extends Component {
+	render() {
+		return(
+			<div> {console.log(this.props)}</div>
+		)
+	}
+}
+
+module.exports = ProjectsSpecificDetails
