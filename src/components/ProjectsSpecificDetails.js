444c9cbac756d61735e7b28301449d0d5cc2741b
Kyle Bonar <kylewb2011@gmail.com>
2017-05-23 15:50:08 -0500

    Flushed out technologies used. Changed verbage. Still deciding on design.

--- src/components/ProjectsSpecificDetails.js
+++ src/components/ProjectsSpecificDetails.js
@@ -7,81 +7,150 @@ class ProjectsSpecificDetails extends Component {
 
 		this.state = {
 			name: "",
-			description: "",
-			imageURL: "",
-			tools: [],
 			allProjects: [
 				{
 					urlName: "tamu_physics",
 					name: "Texas A&M Physics Website",
 					description: " In just two short months, the Physics Department website was given a lot of love by receiving a new server host, an updated backend, a new responsive design, and a plethera of options for staff and faculty members to keep the content updated. This project was completed within a very small team and is the project I currently spend most of my time maintaining and adding additional functionality to.",
 					imageTitle: "",
-					toolsImg: ["PHP-logo.png"],
-					image: "example.png"
+					tools: [
+						{ name: "PHP 5.4", img: "PHP-logo.png" },
+						{ name: "Wordpress", img: "Wordpress-logo.png" },
+						{ name: "MySQL database", img: "MySQL-logo.png" },
+						{ name: "JQuery JavaScript library", img: "JQuery-logo.png" },
+						{ name: "JavaScript", img: "JS-logo.png" },
+						{ name: "Foundation Zurb CSS library", img: "Foundation-logo.png" },
+						{ name: "CSS3", img: "CSS3-logo.png" }
+					],
+					img: "Tamu-Physics-SS.png"
 				},
 				{
 					urlName: "tamu_mitchell",
-					name: "Texas A&M Physics Website",
-					description: "lorem ipsum",
+					name: "Texas A&M Astronomy Website",
+					description: "As a student Web Development Assistant, I worked on a small team to recreate the Astronomy Department's home domain. We were responsible for migrating years of static content into a MySQL database, for both easier managment and record keeping, while also ensuring that every page be responsive. Several custom Wordpress themes were developed for this project and are actively maintained today.",
 					imageTitle: "",
-					toolsImg: [],
-					image: "example.png"
+					tools: [
+						{ name: "PHP 5.4", img: "PHP-logo.png" },
+						{ name: "Wordpress", img: "Wordpress-logo.png" },
+						{ name: "MySQL database", img: "MySQL-logo.png" },
+						{ name: "JQuery JavaScript library", img: "JQuery-logo.png" },
+						{ name: "JavaScript", img: "JS-logo.png" },
+						{ name: "Foundation Zurb CSS library", img: "Foundation-logo.png" },
+						{ name: "CSS3", img: "CSS3-logo.png" }
+					],
+					img: "Tamu-Astronomy-SS.png"
 				},
 				{
 					urlName: "Example 1",
 					name: "Texas A&M Physics Website",
 					description: "lorem ipsum",
 					imageTitle: "",
-					toolsImg: [],
-					image: "example.png"
+					tools: [
+						{ name: "PHP 5.4", img: "PHP-logo.png" },
+						{ name: "Wordpress", img: "Wordpress-logo.png" },
+						{ name: "MySQL database", img: "MySQL-logo.png" },
+						{ name: "JQuery JavaScript library", img: "JQuery-logo.png" },
+						{ name: "JavaScript", img: "JS-logo.png" },
+						{ name: "Foundation Zurb CSS library", img: "Foundation-logo.png" },
+						{ name: "CSS3", img: "CSS3-logo.png" }
+					],
+					img: "example.png"
 				},
 				{
 					urlName: "Example 2",
 					name: "Texas A&M Physics Website",
 					description: "lorem ipsum",
 					imageTitle: "",
-					toolsImg: [],
-					image: "example.png"
+					tools: [
+						{ name: "PHP 5.4", img: "PHP-logo.png" },
+						{ name: "Wordpress", img: "Wordpress-logo.png" },
+						{ name: "MySQL database", img: "MySQL-logo.png" },
+						{ name: "JQuery JavaScript library", img: "JQuery-logo.png" },
+						{ name: "JavaScript", img: "JS-logo.png" },
+						{ name: "Foundation Zurb CSS library", img: "Foundation-logo.png" },
+						{ name: "CSS3", img: "CSS3-logo.png" }
+					],
+					img: "example.png"
 				},
 				{
 					urlName: "Example 3",
 					name: "Texas A&M Physics Website",
 					description: "lorem ipsum",
 					imageTitle: "",
-					toolsImg: [],
-					image: "example.png"
+					tools: [
+						{ name: "PHP 5.4", img: "PHP-logo.png" },
+						{ name: "Wordpress", img: "Wordpress-logo.png" },
+						{ name: "MySQL database", img: "MySQL-logo.png" },
+						{ name: "JQuery JavaScript library", img: "JQuery-logo.png" },
+						{ name: "JavaScript", img: "JS-logo.png" },
+						{ name: "Foundation Zurb CSS library", img: "Foundation-logo.png" },
+						{ name: "CSS3", img: "CSS3-logo.png" }
+					],
+					img: "example.png"
 				},
 				{
 					urlName: "Example 4",
 					name: "Texas A&M Physics Website",
 					description: "lorem ipsum",
 					imageTitle: "",
-					toolsImg: [],
-					image: "example.png"
+					tools: [
+						{ name: "PHP 5.4", img: "PHP-logo.png" },
+						{ name: "Wordpress", img: "Wordpress-logo.png" },
+						{ name: "MySQL database", img: "MySQL-logo.png" },
+						{ name: "JQuery JavaScript library", img: "JQuery-logo.png" },
+						{ name: "JavaScript", img: "JS-logo.png" },
+						{ name: "Foundation Zurb CSS library", img: "Foundation-logo.png" },
+						{ name: "CSS3", img: "CSS3-logo.png" }
+					],
+					img: "example.png"
 				},
 				{
 					urlName: "Example 5",
 					name: "Texas A&M Physics Website",
 					description: "lorem ipsum",
 					imageTitle: "",
-					toolsImg: [],
-					image: "example.png"
+					tools: [
+						{ name: "PHP 5.4", img: "PHP-logo.png" },
+						{ name: "Wordpress", img: "Wordpress-logo.png" },
+						{ name: "MySQL database", img: "MySQL-logo.png" },
+						{ name: "JQuery JavaScript library", img: "JQuery-logo.png" },
+						{ name: "JavaScript", img: "JS-logo.png" },
+						{ name: "Foundation Zurb CSS library", img: "Foundation-logo.png" },
+						{ name: "CSS3", img: "CSS3-logo.png" }
+					],
+					img: "example.png"
 				},
 				{
 					urlName: "Example 6",
 					name: "Texas A&M Physics Website",
 					description: "lorem ipsum",
 					imageTitle: "",
-					toolsImg: [],
-					image: "example.png"
+					tools: [
+						{ name: "PHP 5.4", img: "PHP-logo.png" },
+						{ name: "Wordpress", img: "Wordpress-logo.png" },
+						{ name: "MySQL database", img: "MySQL-logo.png" },
+						{ name: "JQuery JavaScript library", img: "JQuery-logo.png" },
+						{ name: "JavaScript", img: "JS-logo.png" },
+						{ name: "Foundation Zurb CSS library", img: "Foundation-logo.png" },
+						{ name: "CSS3", img: "CSS3-logo.png" }
+					],
+					img: "example.png"
 				},
 				{
 					urlName: "Example 7",
 					name: "Texas A&M Physics Website",
 					description: "lorem ipsum",
 					imageTitle: "",
-					toolsImg: [],
-					image: "example.png"
+					tools: [
+						{ name: "PHP 5.4", img: "PHP-logo.png" },
+						{ name: "Wordpress", img: "Wordpress-logo.png" },
+						{ name: "MySQL database", img: "MySQL-logo.png" },
+						{ name: "JQuery JavaScript library", img: "JQuery-logo.png" },
+						{ name: "JavaScript", img: "JS-logo.png" },
+						{ name: "Foundation Zurb CSS library", img: "Foundation-logo.png" },
+						{ name: "CSS3", img: "CSS3-logo.png" }
+					],
+					img: "example.png"
 				}
 			]
 		}
@@ -114,7 +183,7 @@ class ProjectsSpecificDetails extends Component {
 					return (
 						<div key="project.name" className="specific">
 							<div className="left image-holder">
-								<img src={`/src/images/${project.image}`} style={{height:200, width:200}} title={project.imageTitle}/>
+								<img src={`/src/images/${project.img}`} style={{height:200, width:200}} title={project.imageTitle}/>
 							</div>
 
 							<div className="right text-holder">
@@ -123,13 +192,30 @@ class ProjectsSpecificDetails extends Component {
 								</div>
 
 								<div className="project-description">
-									<h2> Description: </h2>
+									<h2>Description</h2>
 									<p>{project.description}</p>
 								</div>
 
 
-								<div className="project-languages-technologies">
-									{project.map}
+								<div className="project-technologies">
+									<h2>Tools</h2>
+									<p>
+										{project.tools.map( (tool) => {
+											return <img
+												key={tool.name}
+												src={`/src/images/${tool.img}`}
+												className="technology-image"
+												title={tool.name}
+												alt={tool.name} 
+												/>
+										})}
+									</p>
+								</div>
+
+								<div className="project-pictures">
+									<h2>Picture</h2>
+									<div className="pictures-container">
+									</div>
 								</div>
 							</div>
 
