c31c97e631494c09a1434d55098d27af8307fbb7
Kyle Bonar <kylewb2011@gmail.com>
2017-05-24 11:24:45 -0500

    Identical to About component with appropriate class names.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,32 @@
 import React from 'react';
 
 class Home extends React.Component {
+	componentDidMount() {
+		document.title = "Kyle Bonar";
+	}
 	render() {
 		return(
-			<div> Home</div>
+			<div className="home-container">
+				<p className="home-header">					
+					<span className="droid-bold">Hello world!</span> Welcome to my portfolio website.
+				</p>
+
+				<p>
+					I am a <span className="droid-bold">Full Stack Dev</span> who likes to work with JavaScript as much as possible.
+				</p>
+
+				<p>
+					I primarily focus on the front-end, I like to make pixel-perfect, minimalistic designs, but I also enjoy working with databases and server logic. 
+				</p>
+
+				<p>
+					The majority of the updates to this site will be focused on the "Projects" section. Stay tuned!
+				</p>
+
+				<p>
+					- Kyle Bonar
+				</p>
+			</div>
 		)
 	}
 }
