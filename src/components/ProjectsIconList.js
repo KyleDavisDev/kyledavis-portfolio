dec3183e4c5be6d27b2fc3ac047fb42bab7447a5
Kyle Bonar <kylewb2011@gmail.com>
2017-05-25 13:32:03 -0500

    Extracted images out for use with webpack.

--- src/components/ProjectsIconList.js
+++ src/components/ProjectsIconList.js
@@ -1,21 +1,31 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 
+//images
+import TamuPhysicsSS from '../images/Tamu-Physics-SS.png'
+import TamuAstronomySS from '../images/Tamu-Astronomy-SS.png'
+import PingPongSS from '../images/PingPong-SS.png'
+import WordCounterSS from '../images/WordCounter-SS.png'
+import PhyllotaxisSS from '../images/Phyllotaxis-SS.png'
+import FractalTreesSS from '../images/FractalTrees-SS.png'
+import DoublePendulumSS from '../images/DoublePendulum-SS.png'
+import Example from '../images/example.png'
+
 class ProjectsIconList extends Component {
 	constructor(props) {
 		super(props)
 
 		this.state = {
 			icons: [
-				{ displayName: "Physics Department", img: "Tamu-Physics-SS.png", title:"Physics Deparment at Texas A&M", linkURL: "tamu_physics"},
-				{ displayName: "Astronomy Department", img: "Tamu-Astronomy-SS.png", title:"Astronomy Deparment at Texas A&M", linkURL: "tamu_mitchell"},
-				{ displayName: "Classic Ping Pong", img: "PingPong-SS.png", title:"Ping Pong Game", linkURL: "Classic_Ping_Pong"},
-				{ displayName: "Word Counting App", img: "WordCounter-SS.png", title:"Word Counting App", linkURL: "Word_Counting_App"},
-				{ displayName: "Phyllotaxis App", img: "Phyllotaxis-SS.png", title:"Phyllotaxis App", linkURL: "Phyllotaxis_App"},
-				{ displayName: "Fractal Trees App", img: "FractalTrees-SS.png", title:"Fractal Trees App", linkURL: "Fractal_Trees_App"},
-				{ displayName: "Double Pendulum Visual", img: "DoublePendulum-SS.png", title:"Double Pendulum Visualization", linkURL: "Double_Pendulum_Vis"},
-				{ displayName: "Example 8", img: "example.png", title:"Example Title", linkURL: "/"},
-				{ displayName: "Example 9", img: "example.png", title:"Example Title", linkURL: "/"}
+				{ displayName: "Physics Department", img: TamuPhysicsSS, title:"Physics Deparment at Texas A&M", linkURL: "tamu_physics"},
+				{ displayName: "Astronomy Department", img: TamuAstronomySS, title:"Astronomy Deparment at Texas A&M", linkURL: "tamu_mitchell"},
+				{ displayName: "Classic Ping Pong", img: PingPongSS, title:"Ping Pong Game", linkURL: "Classic_Ping_Pong"},
+				{ displayName: "Word Counting App", img: WordCounterSS, title:"Word Counting App", linkURL: "Word_Counting_App"},
+				{ displayName: "Phyllotaxis App", img: PhyllotaxisSS, title:"Phyllotaxis App", linkURL: "Phyllotaxis_App"},
+				{ displayName: "Fractal Trees App", img: FractalTreesSS, title:"Fractal Trees App", linkURL: "Fractal_Trees_App"},
+				{ displayName: "Double Pendulum Visual", img: DoublePendulumSS, title:"Double Pendulum Visualization", linkURL: "Double_Pendulum_Vis"},
+				{ displayName: "Example 8", img: Example, title:"Example Title", linkURL: "/"},
+				{ displayName: "Example 9", img: Example, title:"Example Title", linkURL: "/"}
 			]
 		}
 	}
@@ -24,16 +34,16 @@ class ProjectsIconList extends Component {
 		return(
 			<div className="projects-icon-list-container">
 				{this.state.icons.map( (icon) =>	
-						<NavLink
-							key={icon.displayName}
-							className="project-card"
-							to={{
-								pathname: this.props.match.url + '/details',
-								search: '?title=' + icon.linkURL 
-							}}>
-							<img src={`/src/images/${icon.img}`} style={{height:200, width: 200}} title={icon.title} alt={icon.title}/>
-							{icon.displayName}
-						</NavLink>			
+					<NavLink
+						key={icon.displayName}
+						className="project-card"
+						to={{
+							pathname: this.props.match.url + '/details',
+							search: '?title=' + icon.linkURL 
+						}}>
+						<img src={icon.img} style={{height:200, width: 200}} title={icon.title} alt={icon.title}/>
+						{icon.displayName}
+					</NavLink>			
 				)}
 
 			</div>
