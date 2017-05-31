158dcdab021447c5ec5372217ef2e1f0679ffa03
Kyle Bonar <kylewb2011@gmail.com>
2017-05-31 15:26:50 -0500

    Added old personal website

--- src/components/Projects/All/ProjectsIconList.js
+++ src/components/Projects/All/ProjectsIconList.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom'
 //ScreenShots
 import TamuPhysicsSS from '../../../images/Tamu-Physics-SS.png'
 import TamuAstronomySS from '../../../images/Tamu-Astronomy-SS.png'
+import KyleBonarOriginalWebsite from '../../../images/kyle_bonar_original_website.png'
 import PingPongSS from '../../../images/PingPong-SS.png'
 import WordCounterSS from '../../../images/WordCounter-SS.png'
 import PhyllotaxisSS from '../../../images/Phyllotaxis-SS.png'
@@ -19,6 +20,7 @@ class ProjectsIconList extends Component {
 			icons: [
 				{ displayName: "Physics Department", img: TamuPhysicsSS, title:"Physics Deparment at Texas A&M", linkURL: "tamu_physics"},
 				{ displayName: "Astronomy Department", img: TamuAstronomySS, title:"Astronomy Deparment at Texas A&M", linkURL: "tamu_mitchell"},
+				{ displayName: "Kyle Bonar Original", img: KyleBonarOriginalWebsite, title:"Kyle Bonar original website", linkURL: "kyle_bonar_original"},
 				{ displayName: "Classic Ping Pong", img: PingPongSS, title:"Ping Pong Game", linkURL: "Classic_Ping_Pong"},
 				{ displayName: "Word Counting App", img: WordCounterSS, title:"Word Counting App", linkURL: "Word_Counting_App"},
 				{ displayName: "Phyllotaxis App", img: PhyllotaxisSS, title:"Phyllotaxis App", linkURL: "Phyllotaxis_App"},
