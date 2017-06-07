6437a4b6688d0acc1434d4df9e07631b11e3cdaf
Kyle Bonar <kylewb2011@gmail.com>
2017-06-07 14:08:05 -0500

    Removed unused import and updated component

--- src/components/Projects/All/ProjectsTextTop.js
+++ src/components/Projects/All/ProjectsTextTop.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
+import React  from 'react'
 
-class ProjectsTextTop extends Component {
-	render() {
-		return(
-			<div className="projects-text-container">
-				<h2 className="droid-bold text-center"> My Projects </h2>
+const ProjectsTextTop = () => {
+	return(
+		<div className="projects-text-container">
+			<h2 className="droid-bold text-center"> My Projects </h2>
 
-				<p className="text-center">
-					Check out some of the things that I've created or have been a part of over the years.
-				</p>
-			</div>
-		)
-	}
+			<p className="text-center">
+				Check out some of the things that I've created or have been a part of over the years.
+			</p>
+		</div>
+	)
 }
 
 module.exports = ProjectsTextTop
