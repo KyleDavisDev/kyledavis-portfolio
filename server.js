be5f3cc47cb45f08d8ff288720322a4d2ddb328b
Kyle Bonar <kylewb2011@gmail.com>
2017-06-11 02:24:00 +0000

    Changed port number

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ app.get('*', function(req, res) {
 	res.sendFile(`${process.cwd()}/dist/index.html`);
 });
 
-app.listen(8080, function() {
-	console.log('Example app listening on port 8080!');
+app.listen(5000, function() {
+	console.log('Example app listening on port 5000!');
 })
