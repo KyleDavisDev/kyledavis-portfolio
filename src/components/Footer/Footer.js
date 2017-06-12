c412040f9a67ab70514bbbb9d539836e1f16ddf7
Kyle Bonar <kylewb2011@gmail.com>
2017-06-11 22:20:29 -0500

    Updated to be stateless

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import './Footer.scss'
 
 import Images, { Logos } from '../Images/Images.js'
 
-class Footer extends Component {
-	render() {
-		return (
-			<div className="footer-container">
-				<a href="https://github.com/KyleBonar" target="_blank">
-					<img 
-						src={Logos.GitHubLogo}
-						alt="Kyle Bonar GitHub profile"
-						title="Kyle Bonar GitHub profile"
-						className="footer-image"
-					/>
-				</a>
-				<a href="https://www.linkedin.com/in/kyle-bonar-687276126" target="_blank">
-					<img
-						src={Logos.LinkedInLogo}
-						alt="Kyle Bonar LinkedIn Profile"
-						title="Kyle Bonar LinkedIn Profile"
-						className="footer-image"
-					/>
-				</a>
-			</div>
-		)
-	}
+const Footer = () => {
+	return (
+		<div className="footer-container">
+			<a href="https://github.com/KyleBonar" target="_blank">
+				<img 
+					src={Logos.GitHubLogo}
+					alt="Kyle Bonar GitHub profile"
+					title="Kyle Bonar GitHub profile"
+					className="footer-image"
+				/>
+			</a>
+			<a href="https://www.linkedin.com/in/kyle-bonar-687276126" target="_blank">
+				<img
+					src={Logos.LinkedInLogo}
+					alt="Kyle Bonar LinkedIn Profile"
+					title="Kyle Bonar LinkedIn Profile"
+					className="footer-image"
+				/>
+			</a>
+		</div>
+	)
 }
 
 module.exports = Footer
