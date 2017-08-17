3f22349b266268f5ecd8ed751e90315ed9d375e1
Kyle Bonar <kylewb2011@gmail.com>
2017-08-17 04:00:41 +0000

    Updated to serve static files from inside kylebonar_2015

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 
 app.use(express.static('dist'));
-// app.use(express.static('src/images'));
+app.use(express.static('KyleBonar_2015'));
 // app.set('views', './build');
 
 //Send Resume
