5e572cb7ebcc9c0e0e49cca7dc3396e9a2b918b5
Kyle Bonar <kylewb2011@gmail.com>
2017-10-11 11:45:21 -0500

    Formatting, updating paths.

--- src/components/Images/Images.js
+++ src/components/Images/Images.js
@@ -1,50 +1,51 @@
 const Images = {
-    Screenshots: {
-        CustomWebIcons: require('../../images/Custom-Icons-SS-min.png'),
-        DoublePendulumSS: require('../../images/DoublePendulum-SS-min.png'),
-        FeatureSpotlight: require('../../images/FeatureSpotlight-SS-min.png'),
-        FractalTreesSS: require('../../images/FractalTrees-SS-min.png'),
-        KyleBonarReactWebsite: require('../../images/KyleBonarReactWebsite-SS-min.png'),
-        KyleBonarOriginalWebsite: require('../../images/KyleBonarOriginalWebsite-SS-min.png'),
-        PhyllotaxisSS: require('../../images/Phyllotaxis-SS-min.png'),
-        PingPongSS: require('../../images/PingPong-SS-min.png'),
-        TamuAstronomySS: require('../../images/Tamu-Astronomy-SS-min.png'),
-        TamuPhysicsSS: require('../../images/Tamu-Physics-SS-min.png'),
-        WordCounterSS: require('../../images/WordCounter-SS-min.png')
-    },
-    Specific: {
-        CustomWebIcons: require('/images/Custom-Icons-SS-min.png'),
-        DoublePendulumSS: require('/images/DoublePendulum-SS-min.png'),
-        FeatureSpotlight: require('/images/FeatureSpotlight-SS-min.png'),
-        FractalTreesSS: require('/images/FractalTrees-SS-min.png'),
-        KyleBonarReactWebsite: require('/images/KyleBonarReactWebsite-SS-min.png'),
-        KyleBonarOriginalWebsite: require('/images/KyleBonarOriginalWebsite-SS-min.png'),
-        PhyllotaxisSS: require('/images/Phyllotaxis-SS-min.png'),
-        PingPongSS: require('/images/PingPong-SS-min.png'),
-        TamuAstronomySS: require('/images/Tamu-Astronomy-SS-min.png'),
-        TamuPhysicsSS: require('/images/Tamu-Physics-SS-min.png'),
-        WordCounterSS: require('/images/WordCounter-SS-min.png')
-    },
-    Logos: {
-        AdobeIllustratorLogo: require('../../images/Adobe-Illustrator-CC-Logo.png'),
-        BootstrapLogo: require('../../images/Bootstrap-Logo.png'),
-        CSS3Logo: require('../../images/CSS3-Logo.png'),
-        ExpressLogo: require('../../images/ExpressJS-Logo.png'),
-        FoundationLogo: require('../../images/Foundation-Logo.png'),
-        GitHubLogo: require('../../images/GitHub-Logo.png'),
-        HTML5Logo: require('../../images/HTML5-Logo.png'),
-        JSLogo: require('../../images/JS-Logo.png'),
-        JQueryLogo: require('../../images/JQuery-Logo.png'),
-        LinkedInLogo: require('../../images/LinkedIn-Logo.png'),
-        LinuxLogo: require('../../images/Linux-Logo.png'),
-        MySQLLogo: require('../../images/MySQL-Logo.png'),
-        NginxLogo: require('../../images/Nginx-Logo.png'),
-        NodeJSLogo: require('../../images/NodeJS-Logo.png'),
-        PHPLogo: require('../../images/PHP-Logo.png'),
-        ReactJSLogo: require('../../images/ReactJS-Logo.png'),
-        SassLogo: require('../../images/Sass-Logo.png'),
-        WordPressLogo: require('../../images/Wordpress-Logo.png')
-    }
-}
+  Screenshots: {
+    CustomWebIcons: require("../../images/Custom-Icons-SS-min.png"),
+    DoublePendulumSS: require("../../images/DoublePendulum-SS-min.png"),
+    FeatureSpotlight: require("../../images/FeatureSpotlight-SS-min.png"),
+    FractalTreesSS: require("../../images/FractalTrees-SS-min.png"),
+    KyleBonarReactWebsite: require("../../images/KyleBonarReactWebsite-SS-min.png"),
+    KyleBonarOriginalWebsite: require("../../images/KyleBonarOriginalWebsite-SS-min.png"),
+    PhyllotaxisSS: require("../../images/Phyllotaxis-SS-min.png"),
+    PingPongSS: require("../../images/PingPong-SS-min.png"),
+    TamuAstronomySS: require("../../images/Tamu-Astronomy-SS-min.png"),
+    TamuPhysicsSS: require("../../images/Tamu-Physics-SS-min.png"),
+    WordCounterSS: require("../../images/WordCounter-SS-min.png")
+  },
+  Specific: {
+    CustomWebIcons: "../../../images/Custom-Icons-SS-min.png",
+    CustomWebIcons: "../../../images/Custom-Icons-SS-min.png",
+    DoublePendulumSS: "/../../../images/DoublePendulum-SS-min.png",
+    FeatureSpotlight: "/../../../images/FeatureSpotlight-SS-min.png",
+    FractalTreesSS: "/../../../images/FractalTrees-SS-min.png",
+    KyleBonarReactWebsite: "/../../../images/KyleBonarReactWebsite-SS-min.png",
+    KyleBonarOriginalWebsite: "/../../../images/KyleBonarOriginalWebsite-SS-min.png",
+    PhyllotaxisSS: "/../../../images/Phyllotaxis-SS-min.png",
+    PingPongSS: "/../../../images/PingPong-SS-min.png",
+    TamuAstronomySS: "/../../../images/Tamu-Astronomy-SS-min.png",
+    TamuPhysicsSS: "/../../../images/Tamu-Physics-SS-min.png",
+    WordCounterSS: "/../../../images/WordCounter-SS-min.png"
+  },
+  Logos: {
+    AdobeIllustratorLogo: require("../../images/Adobe-Illustrator-CC-Logo.png"),
+    BootstrapLogo: require("../../images/Bootstrap-Logo.png"),
+    CSS3Logo: require("../../images/CSS3-Logo.png"),
+    ExpressLogo: require("../../images/ExpressJS-Logo.png"),
+    FoundationLogo: require("../../images/Foundation-Logo.png"),
+    GitHubLogo: require("../../images/GitHub-Logo.png"),
+    HTML5Logo: require("../../images/HTML5-Logo.png"),
+    JSLogo: require("../../images/JS-Logo.png"),
+    JQueryLogo: require("../../images/JQuery-Logo.png"),
+    LinkedInLogo: require("../../images/LinkedIn-Logo.png"),
+    LinuxLogo: require("../../images/Linux-Logo.png"),
+    MySQLLogo: require("../../images/MySQL-Logo.png"),
+    NginxLogo: require("../../images/Nginx-Logo.png"),
+    NodeJSLogo: require("../../images/NodeJS-Logo.png"),
+    PHPLogo: require("../../images/PHP-Logo.png"),
+    ReactJSLogo: require("../../images/ReactJS-Logo.png"),
+    SassLogo: require("../../images/Sass-Logo.png"),
+    WordPressLogo: require("../../images/Wordpress-Logo.png")
+  }
+};
 
-module.exports = Images
+module.exports = Images;
