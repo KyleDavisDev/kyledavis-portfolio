a97d2be89d27822923ab475563d686247d808be4
Kyle Bonar <kylewb2011@gmail.com>
2017-10-18 15:58:46 -0500

    Removed a ton of unnecessary content from state and updated render to display appropriately.

--- src/components/Projects/All/ProjectsIconList.js
+++ src/components/Projects/All/ProjectsIconList.js
@@ -1,100 +1,24 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
-import Images, { Screenshots } from "../../Images/Images.js";
+import ProjectList from "../ProjectList";
 
 class ProjectsIconList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      cards: [
-        {
-          displayName: "Feature Spotlight",
-          img: Screenshots.FeatureSpotlight,
-          title: "UTSA Feature Spotlight",
-          linkURL: "feature_spotlight"
-        },
-        {
-          displayName: "ReactJS Portfolio",
-          img: Screenshots.KyleBonarReactWebsite,
-          title: "Kyle Bonar ReactJS Website",
-          linkURL: "kyle_bonar_react"
-        },
-        {
-          displayName: "Physics Department",
-          img: Screenshots.TamuPhysicsSS,
-          title: "Physics Deparment at Texas A&M",
-          linkURL: "tamu_physics"
-        },
-        {
-          displayName: "Astronomy Department",
-          img: Screenshots.TamuAstronomySS,
-          title: "Astronomy Deparment at Texas A&M",
-          linkURL: "tamu_mitchell"
-        },
-        {
-          displayName: "Kyle Bonar Original",
-          img: Screenshots.KyleBonarOriginalWebsite,
-          title: "Kyle Bonar original website",
-          linkURL: "kyle_bonar_original"
-        },
-        {
-          displayName: "Illustrator Web Icons",
-          img: Screenshots.CustomWebIcons,
-          title: "Adobe Illustrator Icons",
-          linkURL: "illustrator_web_icons"
-        },
-        {
-          displayName: "Word Counting App",
-          img: Screenshots.WordCounterSS,
-          title: "Word Counting App",
-          linkURL: "Word_Counting_App"
-        },
-        {
-          displayName: "Phyllotaxis App",
-          img: Screenshots.PhyllotaxisSS,
-          title: "Phyllotaxis App",
-          linkURL: "Phyllotaxis_App"
-        },
-        {
-          displayName: "Fractal Trees App",
-          img: Screenshots.FractalTreesSS,
-          title: "Fractal Trees App",
-          linkURL: "Fractal_Trees_App"
-        },
-        {
-          displayName: "Double Pendulum Visual",
-          img: Screenshots.DoublePendulumSS,
-          title: "Double Pendulum Visualization",
-          linkURL: "Double_Pendulum_Vis"
-        },
-        {
-          displayName: "Classic Ping Pong",
-          img: Screenshots.PingPongSS,
-          title: "Ping Pong Game",
-          linkURL: "Classic_Ping_Pong"
-        }
-      ]
-    };
-  }
-
   render() {
     return (
       <div className="projects-icon-list-container">
-        {this.state.cards.map(icon => (
+        {ProjectList.map(project => (
           <Link
-            key={icon.displayName}
+            key={project.name}
             className="project-card"
-            to={`${this.props.match.url}/details/${icon.linkURL}`}
+            to={`${this.props.match.url}/details/${project.url}`}
           >
             <img
-              src={icon.img}
+              src={project.img}
               className="card-image"
-              title={icon.title}
-              alt={icon.title}
+              title={project.name}
+              alt={project.name}
             />
-            {icon.displayName}
+            {project.name}
           </Link>
         ))}
       </div>
