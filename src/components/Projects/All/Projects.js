bcca02decb286b9564dc42176048f77b951d513e
Kyle Bonar <kylewb2011@gmail.com>
2017-10-19 09:38:07 -0500

    Centralized various components.

--- src/components/Projects/All/Projects.js
+++ src/components/Projects/All/Projects.js
@@ -1,23 +1,57 @@
-import React from 'react';
+import React, {Component} from "react";
+import { Link } from "react-router-dom";
+import ProjectList from "../ProjectList";
 
-import './Projects.scss'
+import "./Projects.scss";
 
-import ProjectsTextTop from './ProjectsTextTop.js'
-import ProjectsIconList from './ProjectsIconList.js'
+const ProjectsTextTop = () => {
+  return (
+    <div className="projects-text-container">
+      <h2 className="droid-bold text-center"> My Projects </h2>
 
-class Projects extends React.Component {
-	componentDidMount() {
-		document.title = "Kyle Bonar - Projects"
-	}
-	render() {
-		return(
-			<div className="projects-container">
-				<ProjectsTextTop />
+      <p className="text-center">
+        Check out some of the things that I've created or have been a part of
+        over the years.
+      </p>
+    </div>
+  );
+};
 
-				<ProjectsIconList match={this.props.match}/>
-			</div>
-		)
-	}
+const ProjectsIconList = props => {
+  return (
+    <div className="projects-icon-list-container">
+      {ProjectList.map(project => (
+        <Link
+          key={project.name}
+          className="project-card"
+          to={`${props.match.url}/details/${project.url}`}
+        >
+          <img
+            src={project.img}
+            className="card-image"
+            title={project.name}
+            alt={project.name}
+          />
+          {project.name}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+class Projects extends Component {
+  componentDidMount() {
+    document.title = "Kyle Bonar - Projects";
+  }
+  render() {
+    return (
+      <div className="projects-container">
+        <ProjectsTextTop />
+
+        <ProjectsIconList match={this.props.match} />
+      </div>
+    );
+  }
 }
 
-module.exports = Projects
+module.exports = Projects;
