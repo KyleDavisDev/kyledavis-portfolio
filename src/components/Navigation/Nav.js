99d325dcdd09d4b316a629c634b5d3bc4b44e9d4
Kyle Bonar <kylewb2011@gmail.com>
2017-11-14 10:21:05 -0600

    Changed class component to stateless functional component.

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -1,37 +1,42 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom'
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
-import './Nav.scss'
+import "./Nav.scss";
 
-class Nav extends React.Component {
-	render() {
+const Nav = () => {
+  return (
+    <nav className="nav-container">
+      <div id="navBar" className="content-container">
+        <NavLink
+          exact
+          className="home-link droid-bold"
+          activeClassName="active"
+          to="/"
+        >
+          Kyle Bonar
+        </NavLink>
 
-		return(
-			<div className="nav-container">
-				<div id="navBar" className="content-container">
-					<NavLink exact className="home-link droid-bold" activeClassName="active" to="/">
-						Kyle Bonar
-					</NavLink>
+        <div id="nav">
+          <ul>
+            <li>
+              <NavLink
+                className="menu-link"
+                activeClassName="active"
+                to="/projects"
+              >
+                Projects
+              </NavLink>
+            </li>
+            <li>
+              <a className="menu-link" target="_blank" href="/resume">
+                Resume
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-					<div id="nav">
-						<ul>
-							<li>
-								<NavLink className="menu-link" activeClassName="active" to="/projects">
-									Projects
-								</NavLink>
-							</li>
-							<li>
-								<a className="menu-link" target="_blank" href="/resume">
-									Resume
-								</a>
-							</li>
-						</ul>
-					</div>
-				</div>
-
-			</div>
-		)
-	}
-}
-
-module.exports = Nav
+module.exports = Nav;
