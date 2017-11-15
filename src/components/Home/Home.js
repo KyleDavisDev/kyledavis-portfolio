15ecfae26d5f6cbd30ca12b363cc37368e252268
Kyle Bonar <kylewb2011@gmail.com>
2017-11-15 15:36:54 -0600

    Broke up components further. Added filler contactme section.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,7 +11,7 @@ const HomeHeader = () => {
       </a>
       <h1 className="droid-bold">Kyle Bonar</h1>
       <h2>
-        Practicing <span className="droid-bold">Full Stack Developer</span>
+        Practicing <span className="droid-bold">UI/UX Developer</span>
       </h2>
       <h2>
         Professional <span className="droid-bold">Coffee Drinker</span>
@@ -20,33 +20,47 @@ const HomeHeader = () => {
   );
 };
 
+const HomeAboutMe = () => {
+  return (
+    <div id="about-me">
+      <h2>About me</h2>
+      <p>
+        <span className="droid-bold">Hello world!</span> Welcome to my portfolio
+        website.
+      </p>
+
+      <p>
+        I am a <span className="droid-bold">Full Stack Dev</span> who likes to
+        work with JavaScript as much as possible.
+      </p>
+
+      <p>
+        I primarily focus on the front-end, I like to make pixel-perfect,
+        minimalistic designs, but I also enjoy working with databases and server
+        logic.
+      </p>
+
+      <p>
+        The majority of the updates to this site will be focused on the
+        "Projects" section. Stay tuned!
+      </p>
+
+      <p>-Kyle Bonar</p>
+    </div>
+  );
+};
+
+const HomeContact = () => {
+  return <div id="contact-me">This will be contact stuff</div>;
+};
+
 const Home = () => {
   return (
     <div className="home-container">
       <HomeHeader />
       <div className="home-content">
-        <p>
-          <span className="droid-bold">Hello world!</span> Welcome to my
-          portfolio website.
-        </p>
-
-        <p>
-          I am a <span className="droid-bold">Full Stack Dev</span> who likes to
-          work with JavaScript as much as possible.
-        </p>
-
-        <p>
-          I primarily focus on the front-end, I like to make pixel-perfect,
-          minimalistic designs, but I also enjoy working with databases and
-          server logic.
-        </p>
-
-        <p>
-          The majority of the updates to this site will be focused on the
-          "Projects" section. Stay tuned!
-        </p>
-
-        <p>-Kyle Bonar</p>
+        <HomeAboutMe />
+        <HomeContact />
       </div>
     </div>
   );
