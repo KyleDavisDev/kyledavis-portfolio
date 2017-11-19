fc9f9b028e6c9056e07dcf2de1d32927869a2148
Kyle Bonar <kylewb2011@gmail.com>
2017-11-18 18:20:54 -0600

    Fixed issue with border bottom not being full width.

--- src/components/Navigation/Nav.js
+++ src/components/Navigation/Nav.js
@@ -3,32 +3,34 @@ import { NavLink, Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav className="nav-container border-bottom">
-      <NavLink
-        exact
-        className="menu-item bold mont upper"
-        activeClassName="active"
-        to="/"
-      >
-        <h3>Kyle Bonar</h3>
-      </NavLink>
+    <nav className="border-bottom">
+      <div className="nav-container">
+        <NavLink
+          exact
+          className="menu-item bold mont upper"
+          activeClassName="active"
+          to="/"
+        >
+          <h3>Kyle Bonar</h3>
+        </NavLink>
 
-      <ul>
-        <li>
-          <NavLink
-            className="menu-item mont upper"
-            activeClassName="active"
-            to="/projects"
-          >
-            <h3>Projects</h3>
-          </NavLink>
-        </li>
-        <li>
-          <a className="menu-item mont upper" target="_blank" href="/resume">
-            <h3>Resume</h3>
-          </a>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <NavLink
+              className="menu-item mont upper"
+              activeClassName="active"
+              to="/projects"
+            >
+              <h3>Projects</h3>
+            </NavLink>
+          </li>
+          <li>
+            <a className="menu-item mont upper" target="_blank" href="/resume">
+              <h3>Resume</h3>
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
