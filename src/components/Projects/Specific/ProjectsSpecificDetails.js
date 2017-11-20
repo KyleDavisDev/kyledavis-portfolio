e3fc138a81e3060eff828966eb039f95600ea5fd
Kyle Bonar <kylewb2011@gmail.com>
2017-11-19 19:36:02 -0600

    Fixed typo, updated DOM structure, removed svg in favor of font-aweome logo, updated component parameter, simplified main component lifecycle.

--- src/components/Projects/Specific/ProjectsSpecificDetails.js
+++ src/components/Projects/Specific/ProjectsSpecificDetails.js
@@ -1,60 +1,52 @@
 import React, { Component } from "react";
 import QueryString from "query-string";
-import Proptypes from "prop-types";
+import PropTypes from "prop-types";
 import ProjectList from "../ProjectList";
+import ArrowRight from "react-icons/lib/fa/arrow-right";
 
 const RenderImage = props => {
   return <img src={props.src} className="project-image" title={props.title} />;
 };
 RenderImage.propTypes = {
-  src: Proptypes.string.isRequired,
-  title: Proptypes.string.isRequired
+  src: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
 };
 
-const RenderTitle = props => {
+const RenderTitle = ({ external, name }) => {
   return (
     <div className="title bold">
-      <a href={props.external} target="_blank" className="linkout">
-        <h2>{props.name}</h2>
-        <svg
-          vectorEffect="non-scaling-stroke"
-          xmlns="http://www.w3.org/2000/svg"
-          width="29"
-          height="30px"
-          viewBox="0 0 1289 1181"
-        >
-          <rect fill="none" x="-1" y="-1" width="1289" height="1181" />
-          <path
-            d="m1010.4 714.2l0 244q0 91.3-60.5 156.1t-145.7 64.8l-595.9 0q-85.2 0-145.7-64.8t-60.5-156.1l0-638.1q0-91.3 60.5-156.1t145.7-64.8l504.2 0q10 0 16.5 6.9t6.4 17.6l0 49.1q0 10.7-6.4 17.6t-16.5 6.9l-504.2 0q-47.3 0-80.9 36t-33.7 86.7l0 638.1q0 50.6 33.7 86.7t80.9 36l595.9 0q47.3 0 80.9-36t33.7-86.7l0-245.4q0-10.7 6.4-17.6t16.5-6.9l45.8 0q10 0 16.5 6.9t6.4 17.9zm275-662.9l0 392.7q0 19.9-13.6 34.5t-32.2 14.6 -32.2-14.6l-126.1-135 -467 500q-7.2 7.7-16.5 7.7t-16.5-7.7l-81.6-87.4q-7.2-7.7-7.2-17.6t7.2-17.6l467-500 -126.1-135q-13.6-14.6-13.6-34.5t13.6-34.5 32.2-14.6l366.7 0q18.6 0 32.2 14.6t13.6 34.5z"
-            fill="#333"
-          />
-        </svg>
+      <a href={external} target="_blank" className="linkout">
+        <h2>{name}</h2>
+        <h3>
+          View <ArrowRight />
+        </h3>
       </a>
     </div>
   );
 };
 RenderTitle.propTypes = {
-  external: Proptypes.string.isRequired
+  external: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired
 };
 
-const RenderDescription = props => {
+const RenderDescription = ({ description }) => {
   return (
     <div className="description">
       <h2>Description</h2>
-      <p>{props.description}</p>
+      <p>{description}</p>
     </div>
   );
 };
 RenderDescription.propTypes = {
-  description: Proptypes.string.isRequired
+  description: PropTypes.string.isRequired
 };
 
-const RenderTools = props => {
+const RenderTools = ({ tools }) => {
   return (
     <div className="technologies">
       <h2>Tools</h2>
       <p>
-        {props.tools.map(tool => {
+        {tools.map(tool => {
           return (
             <img
               key={tool.name}
@@ -70,11 +62,11 @@ const RenderTools = props => {
   );
 };
 RenderTools.propTypes = {
-  tools: Proptypes.arrayOf(
-    Proptypes.shape({
-      name: Proptypes.string.isRequired,
-      img: Proptypes.string.isRequired,
-      name: Proptypes.string.isRequired
+  tools: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      img: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired
     })
   )
 };
@@ -84,7 +76,7 @@ class ProjectsSpecificDetails extends Component {
     super(props);
 
     this.state = {
-      name: ""
+      project: {}
     };
 
     this.getActiveProject = this.getActiveProject.bind(this);
@@ -98,57 +90,46 @@ class ProjectsSpecificDetails extends Component {
     if (activeProject.length === 0) {
       window.location = "/";
     }
-    this.setState({
-      name: urlString
-    });
-  }
-
-  componentDidMount() {
-    const activeProject = this.getActiveProject(this.state.name);
-    document.title = "Kyle Bonar - " + activeProject;
+    this.setState({ project: activeProject });
   }
 
   render() {
+    //destructure state and assign leftovers
+    const { name, img, tools, description, ...project } = this.state.project;
+    //destructure leftovers
+    const { linkPath, externalName } = project.external;
     return (
-      <div className="project-container mw">
-        {ProjectList.filter(project => {
-          return this.state.name === project.url.toLowerCase();
-        }).map(project => {
-          return (
-            <div key={project.name} className="specific">
-              <div className="left image-holder">
-                <a
-                  href={project.external.linkPath}
-                  target="_blank"
-                  title={project.name}
-                  className="image-link"
-                >
-                  <RenderImage src={project.img} title={project.name} />
-                </a>
-              </div>
-
-              <div className="right text-holder">
-                <RenderTitle
-                  external={project.external.linkPath}
-                  name={project.external.name}
-                />
-                <RenderDescription description={project.description} />
-                <RenderTools tools={project.tools} />
-              </div>
-            </div>
-          );
-        })}
+      <div className="container">
+        <div className="bg-lt-splash border-bottom">
+          <div className="mw margin-center p2">
+            <RenderTitle external={linkPath} name={name} />
+          </div>
+        </div>
+        <div id="project-content" className="mw p2 margin-center">
+          <a
+            href={linkPath}
+            target="_blank"
+            title={externalName}
+            className="image-link"
+          >
+            <RenderImage src={img} title={name} />
+          </a>
+          <div id="project-info">
+            <RenderDescription description={description} />
+            <RenderTools tools={tools} />
+          </div>
+        </div>
       </div>
     );
   }
 
   getActiveProject(urlString) {
     //match url query with specific entry in the state data
-    return ProjectList.filter(project => {
-      return urlString === project.url.toLowerCase();
-    }).map(project => {
-      return project.documentTitleName;
-    });
+    for (let i = 0, len = ProjectList.length; i < len; i++) {
+      if (urlString === ProjectList[i].url.toLowerCase()) {
+        return ProjectList[i];
+      }
+    }
   }
 }
 
